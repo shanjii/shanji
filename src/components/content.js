a7930fe88c39129f1f85a8f072c99b77b18ef924
shanjii <victor.galves@live.com>
2021-03-03 01:58:34 -0300

    .

--- src/components/content.js
+++ src/components/content.js
@@ -1,8 +1,11 @@
 import '../styles/content.css'
+window.onresize = () => {
+    document.getElementById("content").style.height = window.innerHeight + 'px'
+}
 
 export function Content() {
     return (
-        <div id="content" style={{ height: '1000px', position: 'relative', top: '100vh'}}>
+        <div id="content" style={{ height: window.innerHeight, position: 'relative', top: '100vh'}}>
             
         </div>
     )
